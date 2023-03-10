da7e1964c41c973d65b350d1d302aaec3574c6eb
ShreyasiDebnath <shreyasidebnath07@gmail.com>
2023-03-11 02:14:22 +0530

    Dsa page added

--- src/App.js
+++ src/App.js
@@ -1,13 +1,22 @@
-
 import './App.css';
+import { Routes, Route } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Home from './components/Home';
+import Dsapage from './components/Dsapage';
 
 function App() {
   return (
    <>
-   <Navbar/>
-   <Home/>
+   < Navbar />
+    <Routes>
+          
+      <Route exact path='/' element={<Home/>}/>
+      <Route exact path='/dsa' element={<Dsapage/>}/>
+      
+    
+          
+       </Routes>
+       
    </>
   );
 }
---
+++ src/components/Dsapage.js
@@ -0,0 +1,51 @@
+import React from 'react'
+import dsal from './Algorithms.jpg';
+import bg from './bg.jpg'
+export default function Dsapage() {
+  return (
+    <>
+    <div className='dsa-page'>
+    <div className='main-side-heading' style={{backgroundImage:`url(${bg})`}}>
+      <div><img src={dsal} className="side-img" alt="..."/></div>
+    </div>
+    <div className='dsa-element'>
+      <div className='dsa-item' id='' href='/'>
+       <a className='items-dsa' href='/'> ARRAY</a>
+      </div>
+      <div className='dsa-item' id='' href='/'>
+       <a className='items-dsa' href='/'> LINKED LIST</a>
+      </div>
+      <div className='dsa-item' id='' href='/'>
+       <a className='items-dsa' href='/'> STACK</a>
+      </div>
+      <div className='dsa-item' id='' href='/'>
+       <a className='items-dsa' href='/'> QUEUE</a>
+      </div>
+      <div className='dsa-item' id='' href='/'>
+       <a className='items-dsa' href='/'> BINARY TREE</a>
+      </div>
+      
+   
+    
+      <div className='dsa-item' id='' href='/'>
+       <a className='items-dsa' href='/'> BINARY SEARCH TREE</a>
+      </div>
+      <div className='dsa-item' id='' href='/'>
+       <a className='items-dsa' href='/'> HEAP</a>
+      </div>
+      <div className='dsa-item' id='' href='/'>
+       <a className='items-dsa' href='/'> HASHING</a>
+      </div>
+      <div className='dsa-item' id='' href='/'>
+       <a className='items-dsa' href='/'> GRAPH</a>
+      </div>
+      <div className='dsa-item' id='' href='/'>
+       <a className='items-dsa' href='/'> DYNAMIC PROGRAMMING</a>
+      </div>
+      
+    </div>
+    </div>
+   
+    </>
+  )
+}
