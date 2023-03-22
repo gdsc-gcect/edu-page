8f2fd06e7573b88ef34d7bf1eb1dc4e35df6e952
ShreyasiDebnath <shreyasidebnath07@gmail.com>
2023-03-22 21:00:56 +0530

    updated

--- src/components/Button.js
+++
@@ -1,9 +0,0 @@
-const Button = ({ onClick, children }) => {
-    return (
-      <button onClick={onClick} className="button">
-        {children}
-      </button>
-    );
-  };
-  
-  export default Button;
