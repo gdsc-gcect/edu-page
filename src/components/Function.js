cb65a830f2ea7cba6f49057fea7c902b8286a8b7
saheli <plsaheli123@gmail.com>
2023-03-26 18:28:01 +0530

    function

--- src/components/Function.js
+++ src/components/Function.js
@@ -131,52 +131,94 @@ export default function PYTHONFundamentals() {
           </div>
         </div>
         <div className="program-que-ans">
-          <h2><center><u>C Loop</u></center></h2>
+          <h2><center><u>Function in C</u></center></h2>
           <div className="program-full-q">
             
            
-            <p>There may be a situation, when you need to execute a block of code several number
-of times. In general, statements are executed sequentially: The first statement in a
-function is executed first, followed by the second, and so on.
-Programming languages provide various control structures that allow for more complicated
-execution paths.
-A loop statement allows us to execute a statement or group of statements multiple times.<br></br>
-Loop:-it is a block of statement that performs set of instructions. In loops
-Repeating particular portion of the program either a specified number of time or
-until a particular no of condition is being satisfied.
-There are three types of loops in c
-<br></br>
-1.While loop<br></br>
-2.do while loop<br></br>
-3.for loop 
-<h3>while loop in C</h3>
+            <p>A function is a self contained block of codes or sub programs with a set of
+statements that perform some specific task or coherent task when it is called.
+It is something like to hiring a person to do some specific task like, every six
+months servicing a bike and hand over to it.
+Any ‘C’ program contain at least one function i.e main().
+There are basically two types of function those are<br></br>
+ <strong><br></br>1. Library function<br></br>
+ 2. User defined function
+ </strong>
+
+
  
  <p>
- A while loop statement in C programming language repeatedly executes a target
-statement as long as a given condition is true.
-Syntax
+ In system defined function description:
+ Function definition : predefined, precompiled, stored in the library<br></br>
+ 
 
-The condition may be any expression, and true is any nonzero value. The loop iterates
-while the condition is true.
-When the condition becomes false, program control passes to the line immediately
-following the loop.
-
+ <strong>Function declaration</strong> : In header file with or function prototype.<br></br>
+ <strong>Function call</strong> : By the programmer<br></br>
+ <strong>User defined function</strong><br></br>
+Syntax:-
+ Return type name of function (type 1 arg 1, type2 arg2, type3 arg3)<br></br>
+ Return type function name argument list of the above syntax
+So when user gets his own function three thing he has to know, these are.<br></br>
+<strong>Function declaration</strong><br></br>
+<strong>Function definition</strong><br></br>
+<strong>Function call</strong><br></br>
  </p>
 
 
+<p>
+ <h4>Function declaration:-</h4>
+ Function declaration is also known as function prototype. It inform the compiler
+about three thing, those are name of the function, number and type of argument
+received by the function and the type of value returned by the function.
+While declaring the name of the argument is optional and the function prototype
+always terminated by the semicolon.
+<h4>Function definition:-</h4>
+Function definition consists of the whole description and code of the function.
+It tells about what function is doing what are its inputs and what are its out put
+It consists of two parts function header and function body
 
-<h3>for loop in C</h3>
-A for loop is a repetition control structure that allows you to efficiently write a loop that
-needs to execute a specific number of times.
+The return type denotes the type of the value that function will return and it is
+optional and if it is omitted, it is assumed to be int by default. The body of the
+function is the compound statements or block which consists of local variable
+declaration statement and optional return statement.
+
+The local variable declared inside a function is local to that function only. It can’t
+be used anywhere in the program and its existence is only within this function.
+The arguments of the function definition are known as formal arguments.
+<h4>Function Call</h4>
+When the function get called by the calling function then that is called, function
+call. The compiler execute these functions when the semicolon is followed by the
+function name.
+
+The argument that are used inside the function call are called actual argument
+
+<h4>Actual argument</h4>
+
+The arguments which are mentioned or used inside the function call is knows as
+actual argument and these are the original values and copy of these are actually
+sent to the called function
+
+<h4>Formal Arguments</h4>
+The arguments which are mentioned in function definition are called formal
+arguments or dummy arguments.
+ 
+These arguments are used to just hold the copied of the values that are sent by the
+calling function through the function call.
+These arguments are like other local variables which are created when the function
+call starts and destroyed when the function ends.
+The basic difference between the formal argument and the actual argument are<br></br>
+ 1. The formal argument are declared inside the parenthesis where as the
+local variable declared at the beginning of the function block.<br></br>
+2. The formal argument are automatically initialized when the copy of actual
+arguments are passed while other local variable are assigned values through the
+statements.
+Order number and type of actual arguments in the function call should be match
+with the order number and type of the formal arguments.
+<h4>Return type</h4>
+It is used to return value to the calling function. 
+
 </p>
-
-<h3>do...while loop in C</h3>
-Unlike for and while loops, which test the loop condition at the top of the loop,
-the do...while loop in C programming language checks its condition at the bottom of the
-loop.
-A do...while loop is similar to a while loop, except that a do...while loop is guaranteed to
-execute at least one time.
-
+ </p>
 
           </div>
         </div>
